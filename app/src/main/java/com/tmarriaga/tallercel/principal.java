b1074eacc78fcd08b5b3448969699b08210095e0
Tommy <tmarriaga@gmail.com>
2018-04-16 18:50:40 -0500

    Archivos iniciales

---
+++ app/src/main/java/com/tmarriaga/tallercel/principal.java
@@ -0,0 +1,13 @@
+package com.tmarriaga.tallercel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
